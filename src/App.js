63b886b46e803e34ddd9f5842963a0c1bd1db237
BrakeLJ <lukejbrake@gmail.com>
2021-07-18 09:52:28 -0400

    added routes / and /search with components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Search from './components/Search';
+import Home from './components/Home';
 
 function App() {
-  return <div>App Placeholder</div>;
+  return (
+    <div>
+      <Router>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
